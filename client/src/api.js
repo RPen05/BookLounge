702d82976c5f34df39ccddf2083d5f3e9667fa86
Tyman3413 <tyman3413@gmail.com>
2023-10-20 18:19:52 +0300

    Работа по клиентской части

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,9 @@
-// api.js
+// API
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000', // Замени на свой базовый URL
-  // Другие настройки Axios, если нужны
+    baseURL: 'http://localhost:3000', // Замени на свой базовый URL
+    // Другие настройки Axios, если нужны
 });
 
 export default apiClient;
